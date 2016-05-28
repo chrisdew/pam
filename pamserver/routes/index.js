355c1e120041e935281d26794ca2a8699d15963b
chrisdew <chris.dew@barricane.com>
2016-05-28 08:05:14 +0100

    closes #4

--- pamserver/routes/index.js
+++ pamserver/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('csv', { title: 'csv2map' });
+  res.render('csv', { title: 'CartoChrono - it\'s map time' });
 });
 
 module.exports = router;
